5d70bddbf8187584ac30d2eeea8a0b0eab65c7d3
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2024-10-18 01:13:06 -0400

    added more routes

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/movies', async (req, res) => {
-    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+app.get('/movies/trending', async (req, res) => {
+    // const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+    const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
     const options = {
         method: 'GET',
         headers: {
@@ -29,7 +30,88 @@ app.get('/movies', async (req, res) => {
             res.json(json)
         })
         .catch(err => console.error('error:' + err));
+})
 
+app.get('/movies/popular', async (req, res) => {
+  const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+      }
+  };
+  
+  const movieRes = await fetch(url, options)
+      .then(response => response.json())
+      .then(json => {
+          console.log(json)
+          res.json(json)
+      })
+      .catch(err => console.error('error:' + err));
+  }
+)
+
+app.get('/movies/top-rated', async (req, res) => {
+  const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+  const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+      }
+  };
+  
+  const movieRes = await fetch(url, options)
+      .then(response => response.json())
+      .then(json => {
+          console.log(json)
+          res.json(json)
+      })
+      .catch(err => console.error('error:' + err));
+  }
+)
+
+app.get('/movies/upcoming', async (req, res) => {
+  // const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+  const url = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1';
+  const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+      }
+  };
+  
+  const movieRes = await fetch(url, options)
+      .then(response => response.json())
+      .then(json => {
+          console.log(json)
+          res.json(json)
+      })
+      .catch(err => console.error('error:' + err));
+})
+
+app.get('/movie/:id/video', async(req, res) => {
+  const id = req.params.id;
+  const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+  const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+      }
+  };
+
+  await fetch(url, options)
+    .then(response => response.json())
+    .then((json) => {
+      console.log(json)
+      res.json(json)
+    })
+    .catch((err) => {
+      console.error("Failed to fetch movie video: ", err);
+    })
 })
 
 app.get('/movie/:id/streaming-options', async (req, res) => {
@@ -46,7 +128,7 @@ app.get('/movie/:id/streaming-options', async (req, res) => {
   const movieRes = await fetch(url, options)
       .then(response => response.json())
       .then(json => {
-          console.log(json)
+          // console.log(json)
           res.json(json)
       })
       .catch(err => console.error('error:' + err));

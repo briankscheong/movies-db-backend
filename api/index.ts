68b1e2bba92b4a197999107ba6b466e07b1bce11
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2025-01-04 17:28:51 -0500

    set up redis server connection and implement logic for caching movie info with 24 hr TTL

--- api/index.ts
+++ api/index.ts
@@ -3,13 +3,44 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const bodyParser = require('body-parser');
+const { createClient } = require('redis');
 
+// const bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser
 // const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(cors({origin: [process.env.FRONTEND_URL, process.env.GITHUB_FRONTEND_URL]}));
+// set up redis client
+const client = createClient({
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_SOCKET_HOST,
+        port: process.env.REDIS_SOCKET_PORT,
+        reconnectStrategy: function(retries) {
+            if (retries > 20) {
+                console.log("Too many attempts to reconnect. Redis connection was terminated");
+                return new Error("Too many retries.");
+            } else {
+                return retries * 500;
+            }
+        }
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+client.connect()
+    .then(() => console.log('Connected to Redis'))
+    .catch(err => console.error('Error connecting to Redis:', err));
+
+app.use(cors({ origin: [process.env.FRONTEND_URL, process.env.GITHUB_FRONTEND_URL] }));
 app.use(express.static('public'));
+
+// Use middleware to attach the Redis client to the request object
+app.use((req, res, next) => {
+    req.redis = client;
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -24,11 +55,11 @@ app.get('/movies/trending', async (req, res) => {
             Authorization: `Bearer ${process.env.BEARER_TOKEN}`
         }
     };
-    
+
     await fetch(url, options)
         .then(response => response.json())
         .then(json => {
-            console.log(json)
+            // console.log(json)
             res.json(json)
         })
         .catch(err => console.error('error:' + err));
@@ -48,7 +79,7 @@ app.get('/movies/popular', async (req, res) => {
     await fetch(url, options)
         .then(response => response.json())
         .then(json => {
-            console.log(json)
+            // console.log(json)
             res.json(json)
         })
         .catch(err => console.error('error:' + err));
@@ -68,7 +99,7 @@ app.get('/movies/top-rated', async (req, res) => {
     await fetch(url, options)
         .then(response => response.json())
         .then(json => {
-            console.log(json)
+            // console.log(json)
             res.json(json)
         })
         .catch(err => console.error('error:' + err));
@@ -88,13 +119,13 @@ app.get('/movies/upcoming', async (req, res) => {
     await fetch(url, options)
         .then(response => response.json())
         .then(json => {
-            console.log(json)
+            // console.log(json)
             res.json(json)
         })
         .catch(err => console.error('error:' + err));
 })
 
-app.get('/movie/:id/video', async(req, res) => {
+app.get('/movie/:id/video', async (req, res) => {
     const id = req.params.id;
     const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
     const options = {
@@ -108,30 +139,52 @@ app.get('/movie/:id/video', async(req, res) => {
     await fetch(url, options)
         .then(response => response.json())
         .then((json) => {
-        console.log(json)
-        res.json(json)
+            // console.log(json)
+            res.json(json)
         })
         .catch((err) => console.error("Failed to fetch movie video: ", err));
 })
 
 app.get('/movie/:id/streaming-options', async (req, res) => {
     const id = req.params.id;
-    const url = `https://streaming-availability.p.rapidapi.com/shows/movie/${id}?series_granularity=show&output_language=en`;
-    const rapid_api_key = process.env.RAPID_API_KEY || "";
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': rapid_api_key,
-            'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
-        }
-    };
+    const cacheId = `movie-info:${id}`;
 
-    await fetch(url, options)
-        .then(response => response.json())
-        .then(json => {
-            res.json(json)
-        })
-        .catch(err => console.error('error:' + err));
+    try {
+        // check if streaming options for a movie is in cache
+        const result = await req.redis.get(cacheId);
+        const json_result = JSON.parse(result);
+        if (json_result !== null) {
+            console.log("cache hit " + cacheId)
+            res.json(result)
+        }
+        else {
+            console.log("cache miss " + cacheId)
+            const url = `https://streaming-availability.p.rapidapi.com/shows/movie/${id}?series_granularity=show&output_language=en`;
+            const rapid_api_key = process.env.RAPID_API_KEY || "";
+            const options = {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-key': rapid_api_key,
+                    'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
+                }
+            };
+    
+            await fetch(url, options)
+                .then(response => response.json())
+                .then(async json => {
+                    // add streaming options result to cache
+                    await req.redis.set(cacheId, JSON.stringify(json))
+                    await req.redis.expire(cacheId, 86400)
+                    console.log("movie " + id + " info cached with key " + cacheId + ". Cache expires in 24 hrs.")
+                    res.json(json)
+                })
+                .catch(err => console.error('error: ' + err));
+        }
+    }
+    catch (err) {
+        console.error('error: ' + err);
+    }
+
 })
 
 app.listen(process.env.PORT || 3001, () => {
